d22013c39d81e236ca3573cf9dd64b4a8aea8714
Turbonator0 <78885232+Turbonator0@users.noreply.github.com>
2022-12-04 15:30:04 +0200

    Day 3 part 1 revamp

--- day_3/01.py
+++ day_3/01.py
@@ -1,12 +1,5 @@
 import string
 
-
-char_array = [  "a","b","c","d","e","f","g","h","i","j","k",
-                "l","m","n","o","p","q","r","s","t","u","v",
-                "w","x","y","z","A","B","C","D","E","F","G",
-                "H","I","J","K","L","M","N","O","P","Q","R",
-                "S","T","U","V","W","X","Y","Z"]
-
 total = 0
 with open("input") as f:
     chars = string.ascii_letters
@@ -15,10 +8,10 @@ with open("input") as f:
         second = line[(int(len(line)/2)):]
         for char in chars:
             if char in first and char in second:
-                for i in range(len(chars)):
-                    if char_array[i] == char:
-                        print(i)
-                        total += i+1
+                if char.islower():
+                    i = ord(char) - 96
+                else: i = ord(char) - 38
 
-
+                total += i
+                    
 print(total)
